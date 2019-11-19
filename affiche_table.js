74a1647a8d3b18d82a0860d14d994e237ba929f6
ItacosI <47180816+ItacosI@users.noreply.github.com>
2019-11-19 14:09:37 +0100

    Add files via upload

--- affiche_table.js
+++ affiche_table.js
@@ -15,52 +15,59 @@ function chercheEnsembleDate(csvjson){ // Renvoie l'ensemble des dates figurant 
 	return res;
 }
 
-function creerBloc(bloc){
-		let res = "<p>"+bloc[1]+ " " + bloc[4] + "/" + bloc[3] + " " + bloc[2] + "</p>\n";
+function creerBloc(bloc, id){
+		let res = "<div class='shadowboxNormal'>\n<Horaire>"+bloc[0]+"</Horaire><Horaire> "+bloc[1]+ "</Horaire>\n</br>\n<lieu>" + bloc[4] + "</lieu></br><button class='boutonAEnfoncer' type='button' name='"+id+"' onclick=swapPanier("+id+")>Choisir</button><br/><titrespectacle>" + bloc[2] + "</titrespectacle>\n</div>\n";
 		return res;
 
 }
 
 function creerTableJour(jour, csvjson){
 	let cpt = 0;
-	let res ="\n<table>\n <th>\n" + jour + "</th>\n <tr>\n  ";
+	let res = "<div class='emballage'>";
 	for (var i = 0; i < csvjson.length; i++) {
 		if(jour == csvjson[i][0]){
-			if(cpt%5 == 4)
-				res += "\n </tr>\n";
+			if(cpt%4 == 3)
+				res += "</div>";
 			cpt++;
-			if(cpt%5 == 0){
-				res += " <tr>\n  ";
+
+			if(cpt%4 == 0){
+				res += "<div class='emballage'>";
 				cpt++;
 			}
-			res+= "<td>"+creerBloc(csvjson[i])+"</td>";
+			
+			res+= creerBloc(csvjson[i], i);
 		}
 	}
-	res+="\n </tr>\n</table>";
+
+	while(cpt < 15){
+		if(cpt%4 == 3)
+			res += "</div>";
+		cpt++;
+
+		if(cpt%4 == 0){
+			res += "<div class='emballage'>";
+			cpt++;
+		}
+		res+="<div class='shadowboxVide'>\n<Horaire></Horaire><Horaire></Horaire>\n</br>\n<lieu></lieu></br><titrespectacle></titrespectacle>\n</div>\n";
+		
+	}
+	res+="</div>";
 	return res;
 }
 
 function appel(csvjson){
-	console.log("res = " + dessineTableau(csvjson));
 	document.getElementById("mod").innerHTML = dessineTableau(csvjson);
 }
 
 function dessineTableau(csvjson){
 	let cpt = 0;
-	let res ="\n<table>\n <tr>\n  ";
+	let res ="\n<div class='decalage'>\n ";
 	let date = chercheEnsembleDate(csvjson);
 
 	for (var i = 0; i < date.length; i++) {
-		if(cpt%3 == 2)
-			res += "\n </tr>\n";
-		cpt++;
-		if(cpt%3 == 0){
-			res += " <tr>\n  ";
-			cpt++;
-		}
-		res+= "<td>"+creerTableJour(date[i], csvjson)+"</td>";
+		res+= "<div class='jourEntier'> <h2>"+date[i]+"</h2>"+creerTableJour(date[i], csvjson)+"</div>";
 	}
-	res+="\n </tr>\n</table>";
+	res+="\n </div>\n";
 	return res;
 }
 
